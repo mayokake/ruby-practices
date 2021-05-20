a4fb44a8137231d7604addd40486726c226d76f5
mayokake <masataka.ikeda@mayokake.com>
2021-05-20 23:47:24 +0900

    modified by Rubocop-san

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,15 +1,17 @@
 #!/Users/masataka_ikeda/.rbenv/versions/3.0.1/bin/ruby
-require "optparse"
-require "date"
+# frozen_string_literal: true
+
+require 'optparse'
+require 'date'
 
 # オプションから引数(年と月）を受け取るためのクラスを定義
 class YearMonth
   def initialize
     @options = {}
     opts = OptionParser.new
-      opts.on("-y [YEAR]") { |year| @options[:y] = year }
-      opts.on("-m [MONTH]") { |month| @options[:m] = month }
-      opts.parse!(ARGV)
+    opts.on('-y [YEAR]') { |year| @options[:y] = year }
+    opts.on('-m [MONTH]') { |month| @options[:m] = month }
+    opts.parse!(ARGV)
   end
 
   def year
@@ -38,13 +40,12 @@ class CalReady
 
   def output_calendar
     puts ("#{@month}月 #{@year}").center(20)
-    puts "日 月 火 水 木 金 土"
-    print " " * 3 * first_day.wday
+    puts '日 月 火 水 木 金 土'
+    print ' ' * 3 * first_day.wday
     (first_day..last_day).each do |date|
       if date.saturday? || date == last_day
-        print date.day.to_s.rjust(2) + " "+ "\n"
-      elsif
-        print date.day.to_s.rjust(2) + " "
+        print "#{date.day.to_s.rjust(2)} \n"
+      else print "#{date.day.to_s.rjust(2)} "
       end
     end
   end
@@ -52,7 +53,7 @@ end
 
 # コマンドラインから引数（年と月）を取得
 # オプションの指定がない場合は、現在の年月を取得
-arguments  = YearMonth.new
+arguments = YearMonth.new
 year = arguments.year
 month = arguments.month
 # カレンダー作成
