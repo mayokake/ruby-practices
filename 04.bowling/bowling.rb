23b8796c1eebd287bf20bdfc5d58a877de65156b
mayokake <masataka.ikeda@mayokake.com>
2021-07-22 17:08:17 +0900

    クラス名・メソッド名変更 pivate化 定数導入 ストライク計算修正 その他

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,41 +1,27 @@
 #!/Users/masataka_ikeda/.rbenv/versions/3.0.1/bin/ruby
 # frozen_string_literal: true
 
-# calculation of your bowling score
-class Bowling
-  def self.calc_points(score)
-    my_score = Bowling.new(score)
-    my_score.total_points
+class BowlingGame
+  LAST_FRAME_INDEX = 9
+
+  def self.total_score(score)
+    bowling_game = BowlingGame.new(score)
+    bowling_game.total_score
   end
 
   def initialize(score)
-    @scores = score[0].split(',').flat_map { |s| s == 'X' ? [10, 0] : s.to_i }
+    @scores = score.split(',').flat_map { |s| s == 'X' ? [10, 0] : s.to_i }
     @basic_score = @scores.each_slice(2).to_a
   end
 
-  def total_points
-    @scores.sum + points_of_strikes + points_of_spares
-  end
-
-  def points_of_strikes
-    ary_for_strike_points.sum
-  end
-
-  def points_of_spares
-    numbers_for_spare_calc.sum { |n| n < 9 ? @basic_score[n + 1][0] : 0 }
+  def total_score
+    @scores.sum + strike_bonus_point + spare_bonus_point
   end
 
   private
 
-  def numbers_for_strike_calc
-    @basic_score.filter_map.with_index do |frame, index|
-      condition_for_strike_calc = frame[0] == 10 && index < 9
-      index if condition_for_strike_calc
-    end
-  end
-
-  def ary_for_strike_points
-    numbers_for_strike_calc.map! do |index|
+  def strike_bonus_point
+    frame_number_for_strike.sum do |index|
       if @basic_score[index + 1][0] == 10
         10 + @basic_score[index + 2][0]
       else
@@ -44,12 +30,23 @@ class Bowling
     end
   end
 
-  def numbers_for_spare_calc
+  def spare_bonus_point
+    frame_number_for_spare.sum { |n| @basic_score[n + 1][0] }
+  end
+
+  def frame_number_for_strike
     @basic_score.filter_map.with_index do |frame, index|
-      condition_for_spare_calc = frame.sum == 10 && frame[1] != 0
+      condition_for_strike_calc = frame[0] == 10 && index < LAST_FRAME_INDEX
+      index if condition_for_strike_calc
+    end
+  end
+
+  def frame_number_for_spare
+    @basic_score.filter_map.with_index do |frame, index|
+      condition_for_spare_calc = frame.sum == 10 && frame[1] != 0 && index < LAST_FRAME_INDEX
       index if condition_for_spare_calc
     end
   end
 end
 
-p Bowling.calc_points(ARGV)
+p BowlingGame.total_score(ARGV[0])
